a4a609666780b6cae37d9e7edb6903bc892ae87a
Francisco Fernandes <francisco.escola1@live.com.pt>
2020-02-08 16:26:33 +0000

    Improvements on search by artist name
    - Bug that happens when searching for more than one artist

--- src/search/search.js
+++ src/search/search.js
@@ -1,38 +1,42 @@
-const spotifyProps = require('../../api/spotify-properties');
+const spotifyClient = require('../../api/spotify-properties').spotifyClient;
+const searchArtistByName = require('./chooseArtist');
 
-function searchArtists(artists){
+function searchArtists(artists, msgDiscord){
     var aux = 0;
     return new Promise ( returnArray => {
         for(var i = 0; i < artists.length; i++){
             new Promise ( res => {
                 res(i);
             }).then( number => {
-                if(artists[number].startsWith("spotify:artist:")){
-                    artists[number] = artists[number].replace("spotify:artist:", "");
-                    aux++;
-                    if(aux === artists.length){
-                        returnArray(artists);
-                    }
-                } else {
-                    new Promise ( resolve => {
-                        spotifyProps.spotifyClient.searchArtists(artists[number], { limit : 20, offset : 0 }).then( dataArtist => {
+                new Promise ( resolve => {
+                    if(artists[number].startsWith("spotify:artist:")){
+                        artists[number] = artists[number].replace("spotify:artist:", "");
+                        resolve();
+                    } else {
+                        spotifyClient.searchArtists(artists[number], { limit : 20, offset : 0 }).then( dataArtist => {
                             if(dataArtist.body.artists.items.length !== 0){
-                                const artistID = dataArtist.body.artists.items[0].id;
-                                artists[number] = artistID;
-                                resolve();
+                                var possibleArtists = dataArtist.body.artists.items;
+                                msgDiscord.reply('Which "_' + artists[number] + '_" are you looking for? (React with ✅ on the desired artist)').then( msg => {
+                                    searchArtistByName.chooseArtist(msgDiscord, possibleArtists, 0).then( artistID => {
+                                        msg.delete().then( () => {
+                                            artists[number] = artistID;
+                                            resolve();
+                                        })
+                                    });
+                                })
                             } else {
                                 artists[number] = artists[number].replace(artists[number], "");
                                 resolve();
                             }
                         })
-                    }).then( () => {
-                        aux++;
-                        if(aux === artists.length){
-                            returnArray(artists);
-                        }
-                    })
                 }
-            }) 
+                }).then( () => {
+                    aux++;
+                    if(aux === artists.length){
+                        returnArray(artists);
+                    }
+                }) 
+            })
         }
     })
 }
