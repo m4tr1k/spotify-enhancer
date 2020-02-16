dc77516bbdcf2404ae2cc95ab73c5cc19362c912
Francisco Fernandes <francisco.escola1@live.com.pt>
2020-02-16 15:51:15 +0000

    Simplified releases code

--- src/releases.js
+++ src/releases.js
@@ -2,113 +2,16 @@ const axios = require('axios');
 const spotify = require('../api/spotify-properties').client;
 const db = require('../api/mongoDB-funcs')
 
-function createMessageNewReleases(artistsIds, channel){
-    var a;
-    var messages = [];
-    for(a = 0; a < artistsIds.length; a++){
-        new Promise ((resolve) => {
-            resolve(a);
-        }).then(a => {
-            var artistID = artistsIds[a];
-            if(artistID === ''){
-                channel.send('**WARNING:** it seems artist number ' + (a+1) + ' does not exist...');
-            } else {
-                spotify.spotifyClient.getArtistAlbums(artistID, {offset: 0, include_groups: 'album,single'}).then( dataAlbum => {
-                    if(dataAlbum.body.items.length === 0){
-                        channel.send('**WARNING:** it seems the artist you chose does not have any release...')
-                    } else {          
-                        axios.get(dataAlbum.body.href, spotify.getAuthOptions()).then( res => {
-                            var resultado = res.data.items;
-                
-                            resultado.sort((a, b) => a.release_date.localeCompare(b.release_date));
-                            resultado.reverse();
-                
-                            const album = resultado[0];
-                            const albumArtists = album.artists.filter(artist => {
-                                if(!album.name.includes(artist.name)){
-                                    return artist.name
-                                }
-                            }).map(artist => artist.name);
-                            
-                            var artistNames = '';
-                
-                            for(var i = 0; i < albumArtists.length - 1; i++){
-                                artistNames += albumArtists[i] + ' & ';
-                            }
-                
-                            artistNames += albumArtists[albumArtists.length - 1];
-                
-                            axios.get(album.href, spotify.getAuthOptions()).then( res => {
-                                var fullAlbum = res.data;
-                
-                                if(fullAlbum.total_tracks > 1){
-                                    var latestRelease = '__**' + artistNames + ' - ' + album.name + '**__\n\nLabel: ' + fullAlbum.label + '\nRelease Date: ';
-                                    const splitDate = album.release_date.split('-');
-                                    const releaseDate = splitDate[2] + '/' + splitDate[1] + '/' + splitDate[0];
-                                    latestRelease += releaseDate + '\n\n';
-
-                                    var tracklist = "";
-                                    const tracks = fullAlbum.tracks.items;
-                    
-                                    for(var i = 0; i < tracks.length; i++){
-                                        tracklist += (i+1) + '. ' + tracks[i].name;
-                                        if(tracks[i].artists.length > 1 && !tracks[i].name.toLowerCase().includes('remix')){
-                                            const artists = tracks[i].artists;
-                                            tracklist += ' (w/ ';
-                                            for(var j = 1; j < artists.length - 1; j++){
-                                                tracklist += artists[j].name + ' & ';
-                                            }
-                                            tracklist += artists[artists.length - 1].name + ')'; 
-                                        }
-                                        tracklist += '\n';
-                                    }
-                    
-                                    latestRelease += 'Tracklist:\n' + tracklist + '\n' + album.external_urls.spotify;
-                                    if(!messages.includes(latestRelease)){
-                                        messages.push(latestRelease);
-                                        channel.send(latestRelease).then( lstMsg => {
-                                            lstMsg.react('👍')
-                                            .then(() => lstMsg.react('👎'))
-                                            .then(() => lstMsg.react('❤️'));
-                                        }); 
-                                    }
-                                } else {
-                                    const trackArtists = fullAlbum.tracks.items[0].artists.filter(artist => {
-                                        if(!albumArtists.includes(artist.name) && !fullAlbum.name.includes(artist.name)){
-                                            return artist.name
-                                        }
-                                    }).map(artist => artist.name);
-
-                                   if(trackArtists.length > 0){
-                                       artistNames += ' (ft. ';
-                                       for(var i = 0; i < trackArtists.length - 1; i++){
-                                           artistNames += trackArtists[i] + ' & ';
-                                       }
-                                       artistNames += trackArtists[trackArtists.length - 1] + ')';
-                                   }
-
-                                    var latestRelease = '__**' + artistNames + ' - ' + album.name + '**__\n\nLabel: ' + fullAlbum.label + '\nRelease Date: ';
-                                    const splitDate = album.release_date.split('-');
-                                    const releaseDate = splitDate[2] + '/' + splitDate[1] + '/' + splitDate[0];
-                                    latestRelease += releaseDate + '\n\n';
-
-                                    latestRelease += fullAlbum.tracks.items[0].external_urls.spotify;
-                                    if(!messages.includes(latestRelease)){
-                                        messages.push(latestRelease);
-                                        channel.send(latestRelease).then( lstMsg => {
-                                            lstMsg.react('👍')
-                                            .then(() => lstMsg.react('👎'))
-                                            .then(() => lstMsg.react('❤️'));
-                                        }); 
-                                    }  
-                                }
-                            })
-                        })
-                    } 
-                })
-            }                
-        })
+async function createMessageNewReleases(artistsIds, channel){
+    const messages = [];
+    for(var i = 0; i < artistsIds.length; i++){
+        const album = await getLatestRelease(artistsIds, i);
+        const msg = await createMessage(album);
+        if(!messages.includes(msg)){
+            messages.push(msg);
+        }
     }
+    sendNewReleases(messages, channel);
 }
 
 async function checkNewReleases(guild){
