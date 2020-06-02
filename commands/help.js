2b2a284ac7ec0edcdcc205bd67db3ed52597e7f8
Francisco Fernandes <1180964@isep.ipp.pt>
2020-06-02 02:11:57 +0100

    Help command updated

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ async function showHelpCommands(msg){
                         .setDescription(description)
                         .addFields({name: "**!SE artists**", value: "List of all artists registered in this server"},
                         {name: "**!SE reset**", value: "Server database reset (command only available for the admin)"},
+                        {name: "**!SE new** *Name of the artist/Spotify URI/Spotify URL, Name of the artist/Spotify URI/Spotify URL...*", value: "Add new releases of an artist without adding it to the server"},
                         {name: "**!SE+** *Name of the artist/Spotify URI/Spotify URL, Name of the artist/Spotify URI/Spotify URL...*", value: "Add one or more artists (up to 20 in a single search) to the server (seperated with commas)"},
                         {name: "**!SE-** *Name of the artist, Name of the artist...*", value: "Remove one or more artists to the server (seperated with commas)"}))
                         
