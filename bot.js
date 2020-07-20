d8f6598a6b7678c674bc9848cfe798ade8e19b0c
Francisco Fernandes <1180964@isep.ipp.pt>
2020-07-20 17:50:40 +0100

    Change authorization user login to client cridentials flow

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
-const express = require('express');
+const startup = require('./startup');
 const discordClient = require('./api/discord-properties').discordClient;
-const spotify = require('./api/spotify-properties').client;
-const auth = require('./auth.json');
 
-const dbNewConnection = require('./api/mongoDB-funcs').newConnection;
 const help = require('./commands/help');
 const checkReleases = require('./src/checkReleases');
 const seeArtists = require('./commands/artists').seeArtists;
@@ -16,67 +13,11 @@ const moveArtistsChannel = require('./commands/moveArtist').moveArtistsChannel;
 const setupReleasesChannel = require('./src/setupReleasesChannel');
 
 const prefix = '!SE';
-
-var app = express();
+startup();
 
 discordClient.on('ready', () => {
-
   discordClient.user.setActivity("!SE help", {type: 'LISTENING'});
-
-  app.get('/', (req, res) => {
-    res.redirect(spotify.getAuthorizeURL());
-  })
-
-  app.get('/callback', (req, res) => {
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-
-    if(state !== null){
-      spotify.spotifyClient.authorizationCodeGrant(code).then(
-        data => {
-          spotify.spotifyClient.setAccessToken(data.body['access_token']);
-          spotify.spotifyClient.setRefreshToken(data.body['refresh_token']);
-
-          console.log(`Spotify connection working...`);
-          dbNewConnection();
-        },
-        err => {
-          console.log('Something went wrong!', err);
-      }).then( () => {
-        var localTime = new Date();
-        if(localTime.getMinutes() === 0){
-          sendNewReleases();
-          setInterval(() => sendNewReleases(), 3600000);
-        } else {
-          localTime.setHours(localTime.getHours() + 1);
-          localTime.setMinutes(0);
-          localTime.setSeconds(0);
-          localTime.setMilliseconds(0);
-          const timeUntilCheck = localTime - new Date();
-          console.log(timeUntilCheck);
-          setTimeout(() => {sendNewReleases(), setInterval(() => sendNewReleases(), 3600000)}, timeUntilCheck);
-        }
-        setInterval(refreshToken, 3600000);
-      })
-    }
-  })
 });
-
-
-function sendNewReleases(){
-  checkReleases.sendNewReleases();
-}
-
-function refreshToken(){
-  spotify.spotifyClient.refreshAccessToken().then(
-    data => {
-      spotify.spotifyClient.setAccessToken(data.body['access_token']);
-    },
-    err => {
-      console.log('Could not refresh access token', err);
-    }
-  );
-}
 
 discordClient.on('message', msg => {
     if(msg.content.startsWith(prefix)) {
@@ -140,7 +81,3 @@ discordClient.on('guildDelete', guild => {
 discordClient.on('channelDelete', channel => {
     setupReleasesChannel.channelDelete(channel);
 })
-
-console.log('Listening on 8888');
-discordClient.login(auth.token);
-app.listen(8888);
