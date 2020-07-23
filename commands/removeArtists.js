58323316d6e046a760e3fc2ecdf16c5103cbe34e
Francisco Fernandes <1180964@isep.ipp.pt>
2020-07-23 21:09:15 +0100

    command removeartists specified on help command and minor adjustments

--- commands/removeArtists.js
+++ commands/removeArtists.js
@@ -1,8 +1,26 @@
 const removeArtists = require('../src/checkReleases').removeArtistsGuild;
 
 async function removeArtistsGuild(msgDiscord, content, cursor){
-    const possibleArtists = content.join(' ').replace('-', "").split(',').map(item => item.trim());
-    removeArtists(possibleArtists, cursor, msgDiscord);
+    const isReleasesCommandsChannel = await cursor.hasNext();
+    if(isReleasesCommandsChannel){
+        if(content.length != 0){
+            const possibleArtists = content.join(' ').split(',').map(item => item.trim());
+            removeArtists(possibleArtists, cursor, msgDiscord);
+        } else {
+            msgDiscord.reply('This command needs arguments (type `!SE help` for more details)');
+        }
+    } else {
+        msgDiscord.reply('You cannot execute this command here!');
+    }
 }
 
-exports.removeArtistsGuild = removeArtistsGuild
+module.exports = {
+    name: '-',
+    title: 'Remove Artist(s)',
+    releasesCommand: true,
+    description: 'Remove an artist that is registered in the server.\nIt is possible to remove several artists using a comma `,`',
+    usage: ['`!SE- name_artist/SpotifyURI/URL, (...)`'],
+    execute(msgDiscord, content, cursor){
+        removeArtistsGuild(msgDiscord, content, cursor);
+    }
+}
